3ce206e08db2ffad7297c8876475cf019e91bab1
NickTaporuk <nictaporuk@yandex.ru>
2013-12-19 11:03:49 +0200

    my 2 probe

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
 <?php
 error_reporting (E_ALL);
-//======================================
-$i=8;
-$factor=2;
-$minimum_limit=0;
-do {
-    if ($i < 5) {
-        echo "i еще недостаточно велико";
-        break;
-    }
-    $i *= $factor;
-    if ($i < $minimum_limit) {
-        break;
-    }
-    echo "значение i уже подходит";
-
-    /* обработка i */
-
-} while (0);
-//======================================
 
 if (version_compare(phpversion(), '5.1.0', '<') == true) { die ('PHP5.1 Only'); }
 require('config/path.php');
